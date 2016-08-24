52b5682d90f235391c69b57daaf56f91953c0ef7
Christopher J Navigato <cnavigato@gmail.com>
2016-08-24 13:33:12 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict'
+/*
+
+npm install express --save
+npm install serve-favicon --save
+*/
+var path = require("path");
+var fs = require("fs");
+
+var favicon = require('serve-favicon');
+var express = require('express');
+var http = require('http');
+
+var app = express();
+var server = http.createServer(app);
+
+app.use(express.static(__dirname + '/public'))
+app.use(favicon(__dirname + '/public/favicon.ico'));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
+
+server.listen(3000);
+console.log('http server now listening on port 3000')
